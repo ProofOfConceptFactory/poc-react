6714d3e73badf64c7a9d0902827941f6738e5536
Abdouni Abdelkarim <abdounikarim@gmail.com>
2024-07-19 05:35:54 +0200

    Test hello

---
+++ tests/hello.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('hello from react', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a h1 "to contain" a substring.
+  await expect(page.locator('h1')).toContainText('Hello from React!👋');
+});
