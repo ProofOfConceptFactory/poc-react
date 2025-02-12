239623c12c9533e1d5d140df8b26c816a5fb4b24
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-02-12 13:08:12 +0100

    HTTPS with vite 🚀

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 8000
+    port: 8000,
+    https: {
+      key: fs.readFileSync('certs/poc-react.key.pem'),
+      cert: fs.readFileSync('certs/poc-react.crt.pem'),
+    },
   }
 })
