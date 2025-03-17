ebda1141c433e9ac1dcd7f8b51026a1991cb16a0
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-03-17 18:46:31 +0100

    Update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from "fs";
+import type { ServerOptions } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  
+  // Définition explicite du type ServerOptions
+  const serverConfig: ServerOptions = {
     host: true,
     port: 8000,
-    https: {
+  };
+  
+  if (isDev) {
+    serverConfig.https = {
       key: fs.readFileSync('certs/poc-react.key.pem'),
       cert: fs.readFileSync('certs/poc-react.crt.pem'),
-    },
+    };
+  }
+  
+  return {
+    plugins: [react()],
+    server: serverConfig
   }
 })
