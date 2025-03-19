468ac5e2439ff7068be258fc27b3aa1e86911b7c
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-03-19 15:17:30 +0100

    Header - add active class

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header(): React.ReactElement {
+    const location = useLocation();
+    const { pathname } = location;
+
     return (
         <>
             <nav>
                 <ul>
                     <li>
-                        <Link to={`/`}>Home</Link>
+                        <Link
+                            to={`/`}
+                            className={pathname === "/" ? "active" : ""}
+                        >
+                            Home
+                        </Link>
                     </li>
                     <li>
-                        <Link to={`/another-page`}>Another page</Link>
+                        <Link
+                            to={`/another-page`}
+                            className={pathname === "/another-page" ? "active" : ""}
+                        >
+                            Another page
+                        </Link>
                     </li>
                 </ul>
             </nav>
